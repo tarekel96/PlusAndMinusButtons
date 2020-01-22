66762fbf55a82c468cbb185225e4a091b0428fdf
Tarek ElHajjaoui <telhajjaoui@Digital-Plus-Tarek-ElHajjaoui.local>
2020-01-22 11:18:12 -0800

    added 0 cap and 100 cap for decrementing and incrementing and started creating a systematic approach for the progressive circle

--- buttons.js
+++ buttons.js
@@ -7,14 +7,60 @@ const incrementAmt = 5,
       plusButton = document.querySelector('#btn-plus'),
       minusButton = document.querySelector('#btn-minus');
 
+/*      
+// sets the border styling for the button
+function initializeButton() {
+    percentageNumber.style.border = "thick solid #000000";
+    percentageNumber.style.borderRadius = '50px';
+    
+}
+
+// initialize button styles
+initializeButton(); 
+*/
+
 // JS way of adjusting percent annuity value
 percentageNumber.innerHTML = `${numberValue} %`;
 
 // increment function for the '+' sign
 function increment(num) {
+
+    // cap for the max that user can set
+    const CAP = 100,
+          BORDER_TOP_LEFT = 20,
+          BORDER_TOP_RIGHT = 45,
+          BORDER_BOT_RIGHT = 70,
+          BORDER_BOT_LEFT = 95,
+          BORDER_EDGE_NUM = '50px';
+    
     
     // prevNum - html string value - convert it to a number
     let prevNum = Number(num);   
+
+    if(num === CAP) {
+        
+        // stringify number back to string data type        
+        strDecimal = String(CAP);   
+        return strDecimal;
+
+    }
+
+    if(num === BORDER_TOP_LEFT) {
+        percentageNumber.style.borderTopLeftRadius = BORDER_EDGE_NUM;
+    }
+
+    if(num === BORDER_TOP_RIGHT) {
+        percentageNumber.style.borderTopRightRadius = BORDER_EDGE_NUM;
+    }
+
+    if(num === BORDER_BOT_RIGHT) {
+        percentageNumber.style.borderBottomRightRadius = BORDER_EDGE_NUM;
+    }
+
+    if(num === BORDER_BOT_LEFT) {
+        percentageNumber.style.borderBottomLeftRadius = BORDER_EDGE_NUM;
+    }
+
     // initialize function variables 
     let newNum; // new number after adding the increment number
     let strDecimal; // variable needed to convert value back to decimal
@@ -34,6 +80,17 @@ plusButton.addEventListener('click', () => {
 
 // increment function for the '+' sign
 function decrement(num) {
+
+    // cap for the min that user can set
+    const CAP = 0;
+
+    if(num === CAP) {
+
+        // stringify number back to string data type        
+        strDecimal = String(CAP);   
+        return strDecimal;
+
+    }
     
     // prevNum - html string value - convert it to a number
     let prevNum = Number(num);   
