b58db3d7b09ef3d7064bff18666fc369af4b3796
Tarek El-Hajjaoui <telhajjaoui@Digital-Plus-Tarek-ElHajjaoui.local>
2020-01-24 11:42:52 -0800

    the buttons work as instructed  syncing now with the percentages

--- button2.js
+++ button2.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
           plusButton2 = document.querySelector('#btn-plus2'),
           minusButton2 = document.querySelector('#btn-minus2');
     
-    let radialObj = radialIndicator('#indicatorContainer', {
+    let radialObj1 = radialIndicator('#indicatorContainer', {
         barColor : '#87CEEB',
         barWidth : 10,
         initValue : 0,
@@ -26,19 +26,17 @@ document.addEventListener('DOMContentLoaded', function () {
     });
      
     // //Using Instance
-    radialObj.animate(); 
+    radialObj1.animate(); 
 
     // //Using Instance
     radialObj2.animate(); 
 
     function updateBar(num) {
-
-        // logic handle synch bar percent numbers
         
         // init vars that handle synch cap logic
         // cap for the max that user can set
         const CAP = 100;
-        let currentBar1Num = radialObj.current_value,
+        let currentBar1Num = radialObj1.current_value,
             currentBar2Num = radialObj2.current_value,
             combinedNum = currentBar1Num + currentBar2Num + increment;
 
@@ -51,19 +49,36 @@ document.addEventListener('DOMContentLoaded', function () {
         else {
             let remainder = 0;
             remainder = 100 - num ;
-            radialObj.animate(num)
+            radialObj1.animate(num)
             radialObj2.animate(remainder);
             return;
         }
 
-
-       
-
-        return radialObj.animate(num);
     }
 
     function updateBar2(num) {
-        return radialObj2.animate(num);
+        
+        // init vars that handle synch cap logic
+        // cap for the max that user can set
+        const CAP = 100;
+        let currentBar1Num = radialObj1.current_value,
+            currentBar2Num = radialObj2.current_value,
+            combinedNum = currentBar1Num + currentBar2Num + increment;
+
+        // ensures the 100 cap is not exceeded by combined bar percents
+        if((combinedNum) === CAP) {   
+            console.log('HERE')           
+            return;
+          } 
+
+        else {
+            let remainder = 0;
+            remainder = 100 - num ;
+            radialObj2.animate(num)
+            radialObj1.animate(remainder);
+            return;
+        }
+
     }
     
     // increment function for the '+' sign
@@ -89,7 +104,7 @@ document.addEventListener('DOMContentLoaded', function () {
     
     // plus click handler
     plusButton1.addEventListener('click', () => {
-        updateBar(increment(radialObj.current_value));   
+        updateBar(increment(radialObj1.current_value));   
          
     })
 
@@ -103,7 +118,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function decrement(num) {
 
         // init vars that handle synch cap logic
-        let currentBar1Num = radialObj.current_value;
+        let currentBar1Num = radialObj1.current_value;
         let currentBar2Num = radialObj2.current_value;
         let combinedNum = currentBar1Num + currentBar2Num;
 
@@ -131,7 +146,7 @@ document.addEventListener('DOMContentLoaded', function () {
     
     // minus click handler
     minusButton1.addEventListener('click', () => {
-        updateBar(decrement(radialObj.current_value));   
+        updateBar(decrement(radialObj1.current_value));   
         })
 
     // minus click handler
